a6118bd23bd82ef12616f504865a8c4214e314fe
Gabriela Muniz <gabrielanamu@live.com>
2023-05-05 17:02:15 -0300

    feat: add req09 - create class Monster

--- src/Character.ts
+++ src/Character.ts
@@ -1,7 +1,7 @@
 import Archetype from './Archetypes/Archetype';
 import Mage from './Archetypes/Mage';
 import Energy from './Energy';
-import Fighter from './Fighter';
+import Fighter, { SimpleFighter } from './Fighter';
 import Race, { Elf } from './Races';
 import getRandomInt from './utils';
 
@@ -57,11 +57,11 @@ export default class Character implements Fighter {
     return { type_: this._energy.type_, amount: this._energy.amount };
   }
 
-  attack(enemy: Fighter): void {
+  attack(enemy: SimpleFighter): void {
     enemy.receiveDamage(this._strength);
   }
 
-  special?(enemy: Fighter): void {
+  special?(enemy: SimpleFighter): void {
     const specialStrength = this._strength * 2;
     enemy.receiveDamage(specialStrength);
   }
